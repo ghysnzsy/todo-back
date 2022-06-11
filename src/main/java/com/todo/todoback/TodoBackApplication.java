67629ea1218c3bea91e3ccf4f00d30c569f0995a
ghysnzsy <30285849+ghysnzsy@users.noreply.github.com>
2022-06-11 15:15:15 +0900

    test입니다.

--- src/main/java/com/todo/todoback/TodoBackApplication.java
+++ src/main/java/com/todo/todoback/TodoBackApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TodoBackApplication {
-
+    //test입니다. Jongyeon
     public static void main(String[] args) {
         SpringApplication.run(TodoBackApplication.class, args);
     }
