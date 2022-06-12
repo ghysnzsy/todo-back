94596381be386a3a329bdc2d7462ca519839ce56
DongWoonKim <01030985843z@naver.com>
2022-06-12 14:50:25 +0900

    테스트페이지 수정

--- src/test/java/com/todo/todoback/domain/TodoMemberTest.java
+++ src/test/java/com/todo/todoback/domain/TodoMemberTest.java
@@ -10,7 +10,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.io.Serializable;
-import java.security.NoSuchAlgorithmException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Optional;
