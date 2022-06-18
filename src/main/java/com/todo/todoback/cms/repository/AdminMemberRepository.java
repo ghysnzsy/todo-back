df47693924ca03e601baa6cefed0fba131a872e5
ghysnzsy <30285849+ghysnzsy@users.noreply.github.com>
2022-06-18 15:51:50 +0900

    어드민 로그인
    폴더 추가, 샘플 파일 추가

---
+++ src/main/java/com/todo/todoback/cms/repository/AdminMemberRepository.java
@@ -0,0 +1,4 @@
+package com.todo.todoback.cms.repository;
+
+public class AdminMemberRepository {
+}
---
+++ src/main/java/com/todo/todoback/cms/service/AdminLoginService.java
@@ -0,0 +1,4 @@
+package com.todo.todoback.cms.service;
+
+public interface AdminLoginService {
+}
